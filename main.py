5729cf91265b8e05a393a99e41a0fcb89ec0f004
unknown <annagibala97@gmail.com>
2024-06-24 22:37:52 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+print("Hello World!")
+
+# Pobieranie danych, hello
+
+z1 = "Hello"
+z2 = input("Proszę podać imię: ")
+
+print(z1 + " " + z2)
+
+# Operatory
+x = 7
+x += 1
+print(x)
+x /= 2
+print(x)
+
+# zaciąganie typów danych
+y = input("Podaj liczbę: ")
+z = input("Podaj liczbę: ")
+print(int(y)+int(z))
